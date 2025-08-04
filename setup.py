cdd30c3094f62f889f4cdc10d3168a6d9d0cba35
swapnilpagar04 <swapnilpagar144@gmail.com>
2025-08-04 11:46:08 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns the list of requirements
+    """
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+        return requirements
+
+setup(
+    name='my_ml_project',
+    version='0.0.1',
+    author='swapnil',
+    author_email='swapnilpagar144@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
